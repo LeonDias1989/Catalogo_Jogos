91f84e31e07f2866c82feb259cfb788c46f1accd
Leon <leondias1989@gmail.com>
2014-12-02 20:36:27 -0200

    Inserindo página principal

---
+++ LEON_trab_prog_web_01/Testes/teste03.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title></title>
+	
+</head>
+<body>
+
+	<header>
+		<iframe src="../html/page_header.php" scrolling="no" align="center" width="100%" height="80px" ></iframe>
+	</header>
+
+</body>
+</html>
---
+++ LEON_trab_prog_web_01/html/page_header.php
@@ -0,0 +1,140 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<title>Catálogo de Jogos</title>
+	<meta charset="UTF-8">
+
+	<style type="text/css">
+		@import "../estilos/inicial.css";
+	</style>
+
+
+</head>
+<body>
+
+	<header>
+		
+		<div class="area">
+			<h1>Catálogo de Jogos</h1>
+		
+			<nav>
+				
+				<ul>
+
+					<li><a href="#">Cadastra-se</a></li>
+					<li><a href="#">Iniciar Sessão</a></li>
+
+				</ul>
+
+		
+			</nav>
+		</div>
+
+	</header>
+
+	<main>
+
+		<section id="titulo">
+
+			<h1>Faça <i>log-on</i> ou cadastre-se</h1>	
+
+		</section>
+
+		
+
+			<section id="log">
+
+				<div class="log">
+
+				<article>
+
+						<form action="../controller/autenticar.php" method="post">
+
+						<fieldset>
+
+						<legend>Entrar</legend>
+			
+						
+						<label>Insira seu e-mail e senha</label>
+
+						<br/><br/>
+						<label for="email">Email:</label>
+						<input name="email" id="email" type="email" required/>
+
+						<br/><br/>
+						<label for="senha">Senha:</label>
+						<input name="senha" id="senha" type="password" required/>
+
+						<br/><br/>
+						<input name="acao" type="submit" value="Entrar" />
+					
+						</fieldset>
+					</form>
+
+				</article>
+
+				<article>
+
+					<form action="../controller/cadastrarUsuario.php" method="post">	
+
+						<fieldset>
+
+							<legend>Cadastrar Usuário</legend>
+
+							<label>Complete os dados do formulário</label>
+							<br/><br/>
+							<label for ="nomeUsuario" >Nome: </label>
+							<input type="text" id="nomeUsuario" name="nomeUsuario">
+							<br/><br/>
+							<label for ="sobreNomeUsuario" >Sobrenome: </label>
+							<input type="text" id="sobreNomeUsuario" name="sobreNomeUsuario">
+							<br/><br/>
+							<label for ="idade" >Idade: </label>	
+							<input type="text" id="idade" name="idade">
+							<br/><br/>
+
+							<label for ="sexo" >Sexo: </label>	
+							<input type="text" id="sexo" name="sexo">	
+							<br/><br/>
+
+							<label for ="email" >E-mail: </label>	
+							<input type="email" id="email" name="email">
+							<br/><br/>
+							<label for ="senha" >Senha: </label>	
+							<input type="password" id="senha" name="senha">					
+
+							<br/><br/>
+
+							<input type="submit" id="enviar" nome="enviar" value="Cadastrar">
+
+						</fieldset>
+
+					</form>
+
+				</article>
+
+				</div>
+			</section>
+
+		
+		
+
+
+	</main>	
+
+
+	<footer>
+
+
+	</footer>
+
+
+
+
+
+
+
+	
+
+</body>
+</html>
