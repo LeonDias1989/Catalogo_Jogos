e63849fc2eaa1f000d3db3ad8cc31e2114511f1e
Leon <leondias1989@gmail.com>
2014-12-08 00:38:45 -0200

    Adicionado atributos à query

--- LEON_trab_prog_web_01/Banco de Dados/query_jogo_usuario.sql
+++ LEON_trab_prog_web_01/Banco de Dados/query_jogo_usuario.sql
@@ -1,1 +1,1 @@
-select nome, distribuidora from jogo inner join jogo_usuario where jogo.id = jogo_usuario.id_jogoFK and jogo_usuario.id_usuarioFK = 13
+select nome, distribuidora, genero, idioma from jogo inner join jogo_usuario where jogo.id = jogo_usuario.id_jogoFK and jogo_usuario.id_usuarioFK = 13
